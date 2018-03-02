5fcb591c65e3d7b1a16c05e5e8223f37b63c16e3
yangyistd <yangyistd@163.com>
2018-03-02 17:18:47 +0800

    登录界面

--- src/ShiYeDetailDAO.py
+++ src/ShiYeDetailDAO.py
@@ -28,17 +28,18 @@ class ShiYeDetailDao():
             self.conn.commit()
             cursor.close()
 
+    # 添加人员数据
     def add_person(self, name, sex, id, month, canbaoTime, fafangTime, endTime, shiye, yiliao, shengyu, shangzhang,
-                   total, cunse, zhangbao, beizhu, item):
+                   total, cunse, zhanghao, beizhu, item):
         cursor = self.conn.cursor()
         try:
             cursor.execute(
                 "INSERT INTO shiye_detail"
                 "(name, sex, id, month, canbaoTime, fafangTime, shiye, yiliao, shengyu, "
                 "shangzhang, total, cunse, zhanghao, beizhu, item, endTime) "
-                "VALUES('" + name + "','" + sex + "','" + id + "','" + id + "','" + month + "','" + canbaoTime + "','" +
-                fafangTime + "','" + shiye + "','" + yiliao + "','" + shengyu + "','" + shangzhang + "','" + total + "','" +
-                cunse + "','" + zhangbao + "','" + beizhu + "','" + item + "','" + endTime + "')")
+                "VALUES('" + name + "','" + sex + "','" + id + "'," + str(month) + ",'" + canbaoTime + "','" +
+                fafangTime + "','" + str(shiye) + "','" + str(yiliao) + "','" + str(shengyu) + "','" + str(shangzhang) + "','" + str(total) + "','" +
+                cunse + "','" + zhanghao + "','" + beizhu + "','" + item + "','" + endTime + "')")
             self.conn.commit()
             return True
         except Exception as e:
@@ -48,6 +49,7 @@ class ShiYeDetailDao():
         finally:
             cursor.close()
 
+    # 根据id删除人员数据
     def delete_user(self, id):
         cursor = self.conn.cursor()
         try:
@@ -60,3 +62,133 @@ class ShiYeDetailDao():
             return False
         finally:
             cursor.close()
+
+    # 更新一个人员数据
+    def update_person_by_id(self, id, name = None, sex = None, month = None, canbaoTime = None, fafangTime = None,
+               shiye = None, yiliao = None, shengyu = None, shangzhang = None, total = None, cunse = None,
+               zhanghao = None, beizhu = None, item = None, endTime = None):
+        cursor = self.conn.cursor()
+        updateSQL = "UPDATE shiye_detail SET "
+        if name is not None:
+            updateSQL = updateSQL + "name='" + name + "', "
+        if sex is not None:
+            updateSQL = updateSQL + "sex='" + sex + "', "
+        if month is not None:
+            updateSQL = updateSQL + "month=" + str(month) + ","
+        if canbaoTime is not None:
+            updateSQL = updateSQL + "canbaoTime='" + canbaoTime + "',"
+        if fafangTime is not None:
+            updateSQL = updateSQL + "fafangTime='" + fafangTime + "',"
+        if shiye is not None:
+            updateSQL = updateSQL + "shiye=" + str(shiye) + ","
+        if yiliao is not None:
+            updateSQL = updateSQL + "yiliao=" + str(yiliao) + ","
+        if shengyu is not None:
+            updateSQL = updateSQL + "shengyu=" + str(shengyu) + ","
+        if shangzhang is not None:
+            updateSQL = updateSQL + "shangzhang=" + str(shangzhang) + ","
+        if total is not None:
+            updateSQL = updateSQL + "total=" + str(total) + ","
+        if cunse is not None:
+            updateSQL = updateSQL + "cunse='" + cunse + "',"
+        if zhanghao is not None:
+            updateSQL = updateSQL + "zhanghao='" + zhanghao + "',"
+        if beizhu is not None:
+            updateSQL = updateSQL + "beizhu='" + beizhu + "',"
+        if item is not None:
+            updateSQL = updateSQL + "item='" + item + "',"
+        if endTime is not None:
+            updateSQL = updateSQL + "endTime='" + item + "',"
+        updateSQL = updateSQL + "id = '" + id + "' "
+        updateSQL = updateSQL + "WHERE id ='" + id + "'"
+        try:
+            cursor.execute(updateSQL)
+            self.conn.commit()
+            return True
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+            return False
+        finally:
+            cursor.close()
+
+    # 根据id查询人员信息
+    def find_by_id(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT name, sex, id, month, canbaoTime, fafangTime, shiye, yiliao, shengyu, "
+                       "shangzhang, total, cunse, zhanghao, beizhu, item, endTime from shiye_detail where "
+                       "id = '" + id + "'")
+        result = cursor.fetchall()
+        person = {}
+        if result:
+            onePerson = result[0]
+            person['name'] = onePerson[0]
+            person['sex'] = onePerson[1]
+            person['id'] = onePerson[2]
+            person['month'] = onePerson[3]
+            person['canbaoTime'] = onePerson[4]
+            person['fafangTime'] = onePerson[5]
+            person['shiye'] = onePerson[6]
+            person['yiliao'] = onePerson[7]
+            person['shengyu'] = onePerson[8]
+            person['shangzhang'] = onePerson[9]
+            person['total'] = onePerson[10]
+            person['cunse'] = onePerson[11]
+            person['zhanghao'] = onePerson[12]
+            person['beizhu'] = onePerson[13]
+            person['item'] = onePerson[14]
+            person['endTime'] = onePerson[15]
+        cursor.close()
+        return person
+
+    # 查询所有的人员数据
+    def find_persons(self):
+        cursor = self.conn.cursor()
+        findSql = "SELECT name, sex, id, month, canbaoTime, fafangTime, shiye, yiliao, shengyu," \
+                  "shangzhang, total, cunse, zhanghao, beizhu, item, endTime from shiye_detail"
+
+        cursor.execute(findSql)
+        result = cursor.fetchall()
+        persons = []
+        if result:
+            for onePerson in result:
+                person = {}
+                person['name'] = onePerson[0]
+                person['sex'] = onePerson[1]
+                person['id'] = onePerson[2]
+                person['month'] = onePerson[3]
+                person['canbaoTime'] = onePerson[4]
+                person['fafangTime'] = onePerson[5]
+                person['shiye'] = onePerson[6]
+                person['yiliao'] = onePerson[7]
+                person['shengyu'] = onePerson[8]
+                person['shangzhang'] = onePerson[9]
+                person['total'] = onePerson[10]
+                person['cunse'] = onePerson[11]
+                person['zhanghao'] = onePerson[12]
+                person['beizhu'] = onePerson[13]
+                person['item'] = onePerson[14]
+                person['endTime'] = onePerson[15]
+                persons.append(person)
+        return persons
+
+def main():
+    conn = sqlite3.connect('JiuYe.db')
+    dao = ShiYeDetailDao(conn)
+    dao.add_person('吴开文', '男', '510521196108236311', 24, '2018/2/1', '2018/3/1', '2018/12/1', 1104, 1104, 1104,
+                   1104, 1104, '长河一组', '6214590482002637905', '信用社', '通滩人民医院消防站项目')
+    dao.add_person('hahaha', '女', '510521196108236312', 24, '2018/2/1', '2018/3/1', '2018/12/1', 1104, 1104, 1104,
+                   1104, 1104, '长河一组', '6214590482002637905', '信用社', '通滩人民医院消防站项目')
+    print(dao.find_by_id("510521196108236311"))
+    print(dao.find_persons())
+    dao.update_person_by_id('510521196108236311', name='周杰伦', sex='男', cunse='台湾省')
+    print(dao.find_persons())
+    dao.delete_user('510521196108236312')
+    print(dao.find_persons())
+
+
+
+
+if __name__ == '__main__':
+    main()
+
--- src/ui.py
+++ src/ui.py
@@ -3,15 +3,21 @@ import sqlite3
 from UserDao import UserDao
 
 
-class UI(object):
+class UI(tkinter.Tk):
 
     def __init__(self, userDao):
+        super().__init__()
+        self.title("Test")
         self.userDao = userDao
-        self.loginBox = tkinter.Tk()
-        self.loginBox.title("登录")
+        self.user = None
+        self.showLoginBox()
 
     def showLoginBox(self):
-        tkinter.mainloop()
+        if not self.user:
+            loginBox = LoginBox(self.userDao)
+            self.wait_window(loginBox)
+            print(loginBox.user)
+
 
     # 用户登录逻辑
     def loginLogic(self, name, password):
@@ -22,13 +28,51 @@ class UI(object):
             print("用户名或密码错误")
 
 
+class LoginBox(tkinter.Toplevel):
+
+    def __init__(self, userDao):
+        super().__init__()
+        self.userDao = userDao
+        self.title('登录')
+        self.setUI()
+        self.resizable(0, 0)
+        self.user = {}
+
+    def setUI(self):
+        row1 = tkinter.Frame(self)
+        row1.grid(row=0)
+        tkinter.Label(row1, text='用户名', width=8).grid(row=0, column=0)
+        self.name = tkinter.StringVar()
+        tkinter.Entry(row1, textvariable = self.name, width=20).grid(row=0, column=1)
+
+        row2 = tkinter.Frame(self)
+        row2.grid(row=1)
+        tkinter.Label(row2, text='密码 ', width=8).grid(row=0, column=0)
+        self.password = tkinter.StringVar()
+        tkinter.Entry(row2, textvariable=self.password, show='*', width=20).grid(row=0, column=1)
+
+        row3 = tkinter.Frame(self)
+        row3.grid(row=2)
+        tkinter.Button(row3, text='登录', command=self.login).grid()
+        # 将登录窗口置于最前
+        self.wm_attributes("-topmost", 1)
+
+    def login(self):
+        user = self.userDao.find_user(self.name.get(), self.password.get())
+        print(user)
+        if user:
+            self.user = user
+            self.destroy()
+        else:
+            print('用户名或密码不正确')
+
+
+
 def main():
     conn = sqlite3.connect('JiuYe.db')
     userDao = UserDao(conn)
     ui = UI(userDao)
-    # ui.showLoginBox()
-    ui.loginLogic("yangyi", "123213")
-    # print(userDao.add_user("yangyi","123213","admin"))
+    ui.mainloop()
 
 
 if __name__ == '__main__':
