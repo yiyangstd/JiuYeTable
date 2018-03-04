d6f704289e16e1ec820ca89ecb9b43af2ccd6e43
yangyi <yangyistd@163.com>
2018-03-04 23:36:33 +0800

    尝试tkinter构建UI，测试后发现不好用，决定采用PyQt

--- src/test.py
+++ src/test.py
@@ -1,80 +1,18 @@
-import tkinter as tk
-'''松耦合'''
-# 弹窗
-class MyDialog(tk.Toplevel):
-  def __init__(self):
-    super().__init__()
-    self.title('设置用户信息')
-    # 弹窗界面
-    self.setup_UI()
-  def setup_UI(self):
-    # 第一行（两列）
-    row1 = tk.Frame(self)
-    row1.pack(fill="x")
-    tk.Label(row1, text='姓名：', width=8).pack(side=tk.LEFT)
-    self.name = tk.StringVar()
-    tk.Entry(row1, textvariable=self.name, width=20).pack(side=tk.LEFT)
-    # 第二行
-    row2 = tk.Frame(self)
-    row2.pack(fill="x", ipadx=1, ipady=1)
-    tk.Label(row2, text='年龄：', width=8).pack(side=tk.LEFT)
-    self.age = tk.IntVar()
-    tk.Entry(row2, textvariable=self.age, width=20).pack(side=tk.LEFT)
-    # 第三行
-    row3 = tk.Frame(self)
-    row3.pack(fill="x")
-    tk.Button(row3, text="取消", command=self.cancel).pack(side=tk.RIGHT)
-    tk.Button(row3, text="确定", command=self.ok).pack(side=tk.RIGHT)
-  def ok(self):
-    self.userinfo = [self.name.get(), self.age.get()] # 设置数据
-    self.destroy() # 销毁窗口
-  def cancel(self):
-    self.userinfo = None # 空！
-    self.destroy()
-# 主窗
-class MyApp(tk.Tk):
-  def __init__(self):
-    super().__init__()
-    #self.pack() # 若继承 tk.Frame ，此句必须有！
-    self.title('用户信息')
-    # 程序参数/数据
-    self.name = '张三'
-    self.age = 30
-    # 程序界面
-    self.setupUI()
-  def setupUI(self):
-    # 第一行（两列）
-    row1 = tk.Frame(self)
-    row1.pack(fill="x")
-    tk.Label(row1, text='姓名：', width=8).pack(side=tk.LEFT)
-    self.l1 = tk.Label(row1, text=self.name, width=20)
-    self.l1.pack(side=tk.LEFT)
-    # 第二行
-    row2 = tk.Frame(self)
-    row2.pack(fill="x")
-    tk.Label(row2, text='年龄：', width=8).pack(side=tk.LEFT)
-    self.l2 = tk.Label(row2, text=self.age, width=20)
-    self.l2.pack(side=tk.LEFT)
-    # 第三行
-    row3 = tk.Frame(self)
-    row3.pack(fill="x")
-    tk.Button(row3, text="设置", command=self.setup_config).pack(side=tk.RIGHT)
-  # 设置参数
-  def setup_config(self):
-    # 接收弹窗的数据
-    res = self.ask_userinfo()
-    #print(res)
-    if res is None: return
-    # 更改参数
-    self.name, self.age = res
-    # 更新界面
-    self.l1.config(text=self.name)
-    self.l2.config(text=self.age)
-  # 弹窗
-  def ask_userinfo(self):
-    inputDialog = MyDialog()
-    self.wait_window(inputDialog) # 这一句很重要！！！
-    return inputDialog.userinfo
+#PyQt测试
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
 if __name__ == '__main__':
-  app = MyApp()
-  app.mainloop()
+    #每一pyqt5应用程序必须创建一个应用程序对象。sys.argv参数是一个列表，从命令行输入参数。
+    app = QApplication(sys.argv)
+    #QWidget部件是pyqt5所有用户界面对象的基类。他为QWidget提供默认构造函数。默认构造函数没有父类。
+    w = QWidget()
+    #resize()方法调整窗口的大小。这离是250px宽150px高
+    w.resize(550, 550)
+    #move()方法移动窗口在屏幕上的位置到x = 300，y = 300坐标。
+    w.move(400, 300)
+    #设置窗口的标题
+    w.setWindowTitle('Simple')
+    #显示在屏幕上
+    w.show()
+    sys.exit(app.exec_())
--- src/ui.py
+++ src/ui.py
@@ -1,32 +1,96 @@
 import tkinter
 import sqlite3
+import tkinter.filedialog
 from UserDao import UserDao
+from tkinter.scrolledtext import ScrolledText
+from tkinter.ttk import Treeview, Scrollbar
 
 
 class UI(tkinter.Tk):
 
     def __init__(self, userDao):
         super().__init__()
-        self.title("Test")
+        self.title("失业人员保险自动计算工具")
         self.userDao = userDao
+        self.protocol('WM_DELETE_WINDOW', self.closeWindow)
         self.user = None
-        self.showLoginBox()
+        self.loginBox = None
+        self.inputFilePath = None
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+        self.geometry('%dx%d+%d+%d' % (1200, 700, (screenwidth - 1200) / 2, (screenheight - 700) / 2))
+        # self.showLoginBox()
+        # if not self.user:
+        #     return
+        frame1 = tkinter.Frame(self, padx=10, pady=10, width=1200, height=100)
+        frame1.grid(row=0, column=0)
+        self.fileEntry = tkinter.Text(frame1, font=("Arial, 16"), state='disabled', height=1, width=95, bd=2)
+        self.fileEntry.grid(row=0, column=0)
+        tkinter.Frame(frame1, width=8).grid(row=0, column=1)
+        fileButton = tkinter.Button(frame1, font=("Arial, 15"), text="打开", command=self.openFile)
+        fileButton.grid(row=0, column=2)
+        tkinter.Frame(frame1, width=8).grid(row=0, column=3)
+        self.inputButton = tkinter.Button(frame1, font=("Arial, 15"), text="导入", state='disabled', command=self.inputFile)
+        self.inputButton.grid(row=0, column=4)
+        frame2 = tkinter.Frame(self, padx=10, width=1055, height=630, bg='red')
+        frame2.grid(row=1, column=0, sticky='W')
+        frame3 = tkinter.Frame(self, padx=10, width=145, height=630, bg='blue')
+        frame3.grid(row=1, column=0, sticky='E')
 
+        # self.showBox = ScrolledText(frame2, font=("Arial, 12"), width=127, height=38)
+        # self.showBox.grid(row=0, column=0)
+
+        # columns = ('c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'
+        #            'c11', 'c12', 'c13', 'c14', 'c15', 'c16')
+        columns = ('c1', 'c2', 'c3', 'c4', 'c5')
+        self.table = Treeview(frame2, columns=columns, height=30, show="headings")
+        ysb = Scrollbar(frame2, orient="vertical", command=self.table.yview())
+        xsb = Scrollbar(frame2, orient="horizontal", command=self.table.xview())
+        self.table.configure(yscroll=ysb.set, xscroll=xsb.set)
+        self.table.grid(row=0, column=0)
+        ysb.grid(row=0, column=1, sticky="ns")
+        xsb.grid(row=1, column=0, sticky="ew")
+        for i in range(100):
+            self.table.insert('', i, values=[str(i)] * 6)
+
+
+
+        # 关闭主窗口相应事件
+    def closeWindow(self):
+        print("close")
+        if self.loginBox:
+            self.loginBox.destroy()
+        self.destroy()
+
+    # 显示登录框
     def showLoginBox(self):
         if not self.user:
             loginBox = LoginBox(self.userDao)
+            self.loginBox = loginBox
             self.wait_window(loginBox)
             print(loginBox.user)
+            if loginBox.user:
+                self.user = loginBox.user
+            else:
+                self.destroy()
 
-
-    # 用户登录逻辑
-    def loginLogic(self, name, password):
-        user = self.userDao.find_user(name, password)
-        if user:
-            print(user)
+    # 打开按钮相应事件
+    def openFile(self):
+        fileName = tkinter.filedialog.askopenfilename(filetypes=[("excel", "*.xls")])
+        self.fileEntry['state'] = tkinter.NORMAL
+        self.fileEntry.delete('0.0', tkinter.END)
+        self.fileEntry.insert(0.0, fileName)
+        self.inputFilePath = fileName
+        self.fileEntry['state'] = tkinter.DISABLED
+        if self.inputFilePath:
+            self.inputButton['state'] = tkinter.NORMAL
         else:
-            print("用户名或密码错误")
+            self.inputButton['state'] = tkinter.DISABLED
 
+    # 导入excel
+    def inputFile(self):
+        if self.inputFilePath:
+            print(self.inputFilePath)
 
 class LoginBox(tkinter.Toplevel):
 
@@ -43,7 +107,7 @@ class LoginBox(tkinter.Toplevel):
         row1.grid(row=0)
         tkinter.Label(row1, text='用户名', width=8).grid(row=0, column=0)
         self.name = tkinter.StringVar()
-        tkinter.Entry(row1, textvariable = self.name, width=20).grid(row=0, column=1)
+        tkinter.Entry(row1, textvariable=self.name, width=20).grid(row=0, column=1)
 
         row2 = tkinter.Frame(self)
         row2.grid(row=1)
@@ -57,15 +121,18 @@ class LoginBox(tkinter.Toplevel):
         # 将登录窗口置于最前
         self.wm_attributes("-topmost", 1)
 
+        screenwidth = self.winfo_screenwidth()
+        screenheight = self.winfo_screenheight()
+        size = '%dx%d+%d+%d' % (220, 80, (screenwidth - 220) / 2, (screenheight - 80) / 2)
+        self.geometry(size)
+
+
     def login(self):
         user = self.userDao.find_user(self.name.get(), self.password.get())
         print(user)
         if user:
             self.user = user
             self.destroy()
-        else:
-            print('用户名或密码不正确')
-
 
 
 def main():
