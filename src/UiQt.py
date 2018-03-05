4f531dfc65dadc2aede78e93161d13a60a547d08
yangyi <yangyistd@163.com>
2018-03-05 22:33:44 +0800

    PyQt版的登录功能

--- src/UiQt.py
+++ src/UiQt.py
@@ -1,24 +1,35 @@
 import sys
-from PyQt5.QtWidgets import (QMainWindow, QDialog, QGridLayout, QPushButton, QLabel, QLineEdit, QDesktopWidget, QApplication)
+import sqlite3
+from UserDao import UserDao
+from PyQt5.QtWidgets import (QMainWindow,QWidget, QDialog, QGridLayout, QPushButton, QLabel, QLineEdit, QDesktopWidget, QMessageBox, QApplication)
 from PyQt5.QtCore import Qt
 
-class MainUI(QMainWindow):
+class MainUI(QWidget):
 
-    def __init__(self):
+    def __init__(self, userDao):
         super().__init__()
+        self.userDao = userDao
         self.user = None
-        self.setGeometry(200, 200, 400, 300)
         self.showLoginBox()
+        # if self.user:
         self.initUI()
 
 
     def showLoginBox(self):
-        loginBox = LoginBox()
+        loginBox = LoginBox(self.userDao)
         loginBox.exec_()
+        self.user = loginBox.user
 
     def initUI(self):
-
+        self.setGeometry(200, 200, 1000, 800)
         self.center()
+        grid = QGridLayout()
+        self.setLayout(grid)
+        self.filePathText = QLineEdit("")
+        self.openButon = QPushButton("打开")
+        grid.addWidget(self.filePathText, 0, 0)
+        grid.addWidget(self.openButon, 0, 3)
+
         self.show()
 
     def center(self):
@@ -30,12 +41,14 @@ class MainUI(QMainWindow):
 
 
 class LoginBox(QDialog):
-    def __init__(self):
+    def __init__(self, userDao):
         super().__init__()
+        self.userDao = userDao
+        self.user = None
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300,300,200,200)
+        self.setGeometry(300,300,250,100)
         self.setWindowTitle("登录")
         grid = QGridLayout()
         self.setLayout(grid)
@@ -49,6 +62,7 @@ class LoginBox(QDialog):
         # 密码框输入返显
         self.passLineEdit.setEchoMode(QLineEdit.Password)
         button = QPushButton("登录")
+        button.clicked.connect(self.login)
         grid.addWidget(nameLabel, 0, 0)
         grid.addWidget(self.nameLineEdit, 0, 1, 1, 2)
         grid.addWidget(passwdLabel, 1, 0)
@@ -58,7 +72,11 @@ class LoginBox(QDialog):
         self.show()
 
     def login(self):
-        print("登录")
+        self.user = self.userDao.find_user(self.nameLineEdit.text(), self.passLineEdit.text())
+        if self.user:
+            self.destroy()
+        else:
+            QMessageBox.information(self, "登录失败", "用户名或密码错误，\n请检查后重新登录")
 
     # 将登录窗口移动到中心
     def center(self):
@@ -70,5 +88,7 @@ class LoginBox(QDialog):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    box = MainUI()
+    conn = sqlite3.connect('JiuYe.db')
+    userDao = UserDao(conn)
+    box = MainUI(userDao)
     app.exit(app.exec_())
